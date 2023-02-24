71614da31945af0c225c32a9ce5220b6c3652710
khyleflores <khyleflores10@gmail.com>
2023-02-24 19:11:11 +0000

    Employee class with 3 properties and 4 methods added

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,21 @@
-// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(employeeName, employeeID, email) {
+      this.employeeName = employeeName;
+      this.employeeID = employeeID;
+      this.email = email;
+    }
+    getName() {
+        return this.employeeName;
+    }
+    getId() {
+        return this.employeeID;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getRole() {
+        return 'Employee';
+    }
+  }
+
+  module.exports = Employee;

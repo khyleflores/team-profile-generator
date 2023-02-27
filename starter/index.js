b0d5e879ebcf838cfa1e4c67d3c9462718bb4e5a
khyleflores <khyleflores10@gmail.com>
2023-02-27 16:59:43 +0000

    Prompted user using inquirer

--- starter/index.js
+++ starter/index.js
@@ -12,4 +12,45 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Prompt questions to users using Inquirer npm
+const promptUser = () =>
+  inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter your name.',
+    },
+    {
+        type: 'input',
+        name: 'empployeeID',
+        message: 'Please enter your employee ID.',
+    },
+    {
+      type: 'input',
+      name: 'emailAddress',
+      message: 'Please enter your email address.',
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Please enter your office number.',
+    }
+]);
+
+// function to initialize program
+// using async/await and try/catch
+const init = async () => {
+    console.log('Hello manager, welcome to the Team Profile Generator.');
+    try {
+     //wait until the promise is settled
+      const answers = await promptUser();
+      
+  
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+// function call to initialize program
+init();
 
